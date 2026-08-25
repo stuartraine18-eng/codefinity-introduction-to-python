f8b19113eeac4131046f253d39025db71bae96da
stuartraine18-eng <>
2026-08-25 14:05:07 +0000

    String Indexing

---
+++ variables_and_types/string_indexing/main.py
@@ -0,0 +1,9 @@
+grocery_item = "Grilled Chicken Salad"
+
+
+
+
+# Testing
+print("Length of item name:", length_of_item)
+print("First character of each word:", first_char, second_char, third_char)
+print("Last character of each word:", last_char1, last_char2, last_char3)
