1e1fc7f23c55dfc9062667d3cffd34e8704eb169
stuartraine18-eng <>
2026-08-27 09:49:02 +0000

    String Indexing

--- variables_and_types/string_indexing/main.py
+++ variables_and_types/string_indexing/main.py
@@ -1,6 +1,13 @@
 grocery_item = "Grilled Chicken Salad"
 
+length_of_item = len(grocery_item)
+first_char = grocery_item[0]
+second_char = grocery_item[8]
+third_char = grocery_item[16]
 
+last_char1 = grocery_item[6]
+last_char2 = grocery_item[14]
+last_char3 = grocery_item[20]
 
 
 # Testing
